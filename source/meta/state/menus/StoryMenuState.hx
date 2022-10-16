852dc33e4a187a1ec1d6b6a79040d5ab32f46abb
SantiYea <95106797+SantiYea@users.noreply.github.com>
2022-10-15 23:07:21 -0300

    update StoryMenuState.hx

--- source/meta/state/menus/StoryMenuState.hx
+++ source/meta/state/menus/StoryMenuState.hx
@@ -23,7 +23,7 @@ class StoryMenuState extends MusicBeatState
 	var scoreText:FlxText;
 	var curDifficulty:Int = 1;
 
-	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true];
+	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true, true];
 
 	var weekCharacters:Array<Dynamic> = [
 		['', 'bf', 'gf'],
