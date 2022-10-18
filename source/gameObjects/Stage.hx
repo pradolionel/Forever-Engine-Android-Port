506a2651f721eaf493fa109caa34a49f668e4217
SantiYea <95106797+SantiYea@users.noreply.github.com>
2022-10-18 03:19:09 -0300

    update Stage.hx

--- source/gameObjects/Stage.hx
+++ source/gameObjects/Stage.hx
@@ -361,7 +361,7 @@ class Stage extends FlxTypedGroup<FlxBasic>
         add(ground);
         
        var Mesita = new FNFSprite(-600, -300).loadGraphic(Paths.image('backgrounds/' + curStage + '/table'));
-        Mesita.cameras = [camOther];
+        PlayState.cameras.Mesita = [camGame];
         Mesita.scrollFactor.set(1.2, 1.2);
         add(Mesita);
        
